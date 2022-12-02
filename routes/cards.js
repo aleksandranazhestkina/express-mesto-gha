d21751d57752c5a31dc85ddc13b53ac34f793800
Aleksandra Nazhestkina <splinatus@yandex.ru>
2022-12-02 15:41:17 +0300

    test10

--- routes/cards.js
+++ routes/cards.js
@@ -16,7 +16,7 @@ router.post('/cards', celebrate({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-/]))?/),
   }),
-}),createCard);
+}), createCard);
 
 router.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
