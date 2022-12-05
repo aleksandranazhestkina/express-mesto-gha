a21f1e799c800cd592db74d2f134b949786248d8
Aleksandra Nazhestkina <splinatus@yandex.ru>
2022-12-05 19:04:57 +0300

    review3.2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,8 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Необходима авторизация.'));
+    next(new UnauthorizedError('Необходима авторизация.'));
+    return;
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,7 +16,8 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return next(new UnauthorizedError('Необходима авторизация.'));
+    next(new UnauthorizedError('Необходима авторизация.'));
+    return;
   }
   req.user = payload;
 
