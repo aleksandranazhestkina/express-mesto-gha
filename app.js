a1cc892c924371c29bf0c8c184105752160f9509
Aleksandra Nazhestkina <splinatus@yandex.ru>
2022-12-18 16:56:41 +0300

    likes, owner

--- app.js
+++ app.js
@@ -18,22 +18,22 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
-// const options = {
-//   origin: [
-//     'http://localhost:3000',
-//     'https://api.nazhestkina.nomoredomains.club',
-//     'https://github.com/aleksandranazhestkina/express-mesto-gha.git',
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-//   credentials: true,
-// };
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://api.nazhestkina.nomoredomains.club',
+    'https://github.com/aleksandranazhestkina/express-mesto-gha.git',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 const app = express();
 
-app.use('*', cors());
+app.use('*', cors(options));
 app.use(helmet());
 app.use(limiter);
 
--- controllers/cards.js
+++ controllers/cards.js
@@ -5,8 +5,7 @@ const ForbiddenError = require('../errors/forbidden-error');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .populate(['owner', 'likes'])
-    .then((cards) => res.send(cards))
+    .then((cards) => res.send(cards.reverse()))
     .catch(next);
 };
 
