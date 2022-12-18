9e9b24c224f9750fe8eb3675382270a27f7df824
Aleksandra Nazhestkina <splinatus@yandex.ru>
2022-12-18 11:47:52 +0300

    2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const { Аuthorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!Аuthorization || !Аuthorization.startsWith('Bearer ')) {
     next(new UnauthorizedError('Необходима авторизация.'));
     return;
   }
 
-  const token = authorization.replace('Bearer ', '');
+  const token = Аuthorization.replace('Bearer ', '');
   let payload;
 
   try {
